ebc52fcb3fe625fdf45ee3d97d4e1c8c09ac73af
timwilliam <19308063+timwilliam@users.noreply.github>
2024-04-08 20:03:31 +0800

    Change keymap for Neotree so that it does not overlap with keymap for tmux

--- init.lua
+++ init.lua
@@ -105,8 +105,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- Keybinds for neo-tree
-vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>', { desc = 'Neotree toggle' })
+-- Keybinds for Neotree
+vim.keymap.set('n', '<leader>b', '<Cmd>Neotree float toggle<CR>', { desc = 'Toggle Neotree' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
