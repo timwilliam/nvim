e9141d6c90a5b5816b97c3e9ec6cfac826dc5b88
timwilliam <19308063+timwilliam@users.noreply.github>
2024-05-18 15:33:26 +0800

    Move custom keymaps and options to init.lua in custom/plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,10 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
+-- Custom keymaps
+
+-- Gitsigns
+vim.keymap.set('n', '<leader>gh', ':Gitsigns preview_hunk<CR>', { desc = 'Gitsigns preview [H]unk' })
+vim.keymap.set('n', '<leader>gi', ':Gitsigns preview_hunk_inline<CR>', { desc = 'Gitsigns preview hunk [I]nline' })
+
+-- Neotree
+vim.keymap.set('n', '<leader>b', '<Cmd>Neotree float toggle<CR>', { desc = 'Toggle Neotree pane [B]' })
+
 return {}
