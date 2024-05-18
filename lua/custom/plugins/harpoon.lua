ce0cb85b9dbd79207f6a102cdfe9f29a89bcc756
timwilliam <19308063+timwilliam@users.noreply.github>
2024-05-18 16:08:54 +0800

    Add harpoon plugin for easier file switching

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,61 @@
+return {
+  'theprimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('harpoon'):setup()
+  end,
+
+  keys = {
+    {
+      '<leader>A',
+      function()
+        require('harpoon'):list():append()
+      end,
+      desc = '[A]dd file to Harpoon',
+    },
+    {
+      '<leader>a',
+      function()
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = 'Toggle H[a]rpoon quick menu',
+    },
+    {
+      '<leader>1',
+      function()
+        require('harpoon'):list():select(1)
+      end,
+      desc = 'Harpoon to file [1]',
+    },
+    {
+      '<leader>2',
+      function()
+        require('harpoon'):list():select(2)
+      end,
+      desc = 'Harpoon to file [2]',
+    },
+    {
+      '<leader>3',
+      function()
+        require('harpoon'):list():select(3)
+      end,
+      desc = 'Harpoon to file [3]',
+    },
+    {
+      '<leader>4',
+      function()
+        require('harpoon'):list():select(4)
+      end,
+      desc = 'Harpoon to file [4]',
+    },
+    {
+      '<leader>5',
+      function()
+        require('harpoon'):list():select(5)
+      end,
+      desc = 'Harpoon to file [5]',
+    },
+  },
+}
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,4 +1,4 @@
--- Custom keymaps
+-- NOTE: Custom keymaps
 
 -- Gitsigns
 vim.keymap.set('n', '<leader>gh', ':Gitsigns preview_hunk<CR>', { desc = 'Gitsigns preview [H]unk' })
@@ -6,5 +6,12 @@ vim.keymap.set('n', '<leader>gi', ':Gitsigns preview_hunk_inline<CR>', { desc = 
 
 -- Neotree
 vim.keymap.set('n', '<leader>b', '<Cmd>Neotree float toggle<CR>', { desc = 'Toggle Neotree pane [B]' })
+
+-- NOTE: Custom options
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 return {}
